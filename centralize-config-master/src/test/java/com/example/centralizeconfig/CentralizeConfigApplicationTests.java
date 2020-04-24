2485a9b948aac40925e46830cb3b411e60ab1f09
loint <nguyentriloi.it@gmail.com>
2020-04-24 09:06:27 +0700

    add config

---
+++ centralize-config-master/src/test/java/com/example/centralizeconfig/CentralizeConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.centralizeconfig;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CentralizeConfigApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
